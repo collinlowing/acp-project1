359b8c2463add1142ab15026d324ab5e4b0c7c7c
cel19 <70173412+cel19@users.noreply.github.com>
2021-09-24 00:32:31 +0000

    Creates random object class Camera

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ class Main {
   public static void main(String[] args) {
     Camera cam = new Camera("Brand", "Model", 12, 100);
     System.out.println(cam);
+
+    Camera cam1 = CameraFactory.createRandomCamera();
+    System.out.println(cam1);
   }
 }
